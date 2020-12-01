a32a8681cc8044e48e083f1644116bd002ce10c6
Louis Michael Medina <viewsdevelop@gmail.com>
2020-12-01 05:20:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,11 +23,11 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return age >= 13 && age <= 19 ?
+  return (age >= 13 && age <= 19) ?
   "You are a teenager" :
   "You are not a teenager" 
 }
 
 function switchAge(age) {
-
+  
 }
