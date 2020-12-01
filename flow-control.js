0f412a87742d3842fe4e829d02d9e27f954e6a08
Louis Michael Medina <viewsdevelop@gmail.com>
2020-12-01 05:16:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,8 +7,7 @@ function basicTeenager(age) {
 function teenager(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!"
-  }
-  else {
+  } else {
     return "You are not a teenager!"
   }
 }
