f73bed8fa592c77d7282432f193110dfe72feb41
Louis Michael Medina <viewsdevelop@gmail.com>
2020-12-01 05:26:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,8 +31,8 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case (age >= 13 && age <= 19):
-      return "You are a teenager";
+      return "You are a teenager"
     default:
-      return "You have an age";
+      return "You have an age"
   }
 }
